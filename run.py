50adc3d19d92bb11662841a6a680d7d534e72f6b
Luis Capelo <luiscape@gmail.com>
2015-10-08 11:47:52 -0400

    adding scheduler

--- run.py
+++ run.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
+import schedule
 import app.main as App
+from app.utilities.format import item
 
-App.Main()
+#
+# Setting-up schedule.
+#
+schedule.every(30).seconds.do(App.Main)
+
+def Main(verbose=True):
+  '''Wrapper to run all the scheduled tasks.'''
+
+  if verbose:
+    print '%s Running scheduler.' % item('bullet')
+
+  try:
+    while True:
+      schedule.run_pending()
+      time.sleep(1)
+
+  except Exception as e:
+    print e
+    return False
+
+
+if __name__ == '__main__':
+  Main()

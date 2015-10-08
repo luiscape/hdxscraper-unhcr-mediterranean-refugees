f7e217ceef114d0c7897e142a6fae171717b2ad2
Luis Capelo <luiscape@gmail.com>
2015-10-08 11:50:42 -0400

    adding scraperwiki notifier. closes #2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
+import scraperwiki
 import collect.fetch as Fetch
 import collect.patch as Patch
 from utilities.db import CleanTable, StoreRecords
@@ -10,25 +10,33 @@ from utilities.format import item
 def Main():
   '''Wrapper.'''
 
-  #
-  # Collecting data from UNHCR.
-  #
-  print '%s Collecting data from UNHCR.' % item('bullet')
-  data = Fetch.Fetch()
+  try:
+    #
+    # Collecting data from UNHCR.
+    #
+    print '%s Collecting data from UNHCR.' % item('bullet')
+    data = Fetch.Fetch()
 
-  #
-  # Patching data.
-  # Epoch time doesn't seem to be 1970.
-  #
-  print '%s Patching data.' % item('bullet')
-  # pdata = Patch.Epoch(data)
-  pdata = Patch.Date(data)
+    #
+    # Patching data.
+    # Epoch time doesn't seem to be 1970.
+    #
+    print '%s Patching data.' % item('bullet')
+    # pdata = Patch.Epoch(data)
+    pdata = Patch.Date(data)
 
-  #
-  # Storing data in database.
-  #
-  print '%s Storing records in database.' % item('bullet')
-  CleanTable('monthly_arrivals_by_country')
-  StoreRecords(pdata, 'monthly_arrivals_by_country')
+    #
+    # Storing data in database.
+    #
+    print '%s Storing records in database.' % item('bullet')
+    CleanTable('monthly_arrivals_by_country')
+    StoreRecords(pdata, 'monthly_arrivals_by_country')
 
-  print '%s Collected data from UNHCR successfully.' % item('success')
+    print '%s Collected data from UNHCR successfully.' % item('success')
+    scraperwiki.status('ok')
+
+  except Exception as e:
+    print '%s UNHCR Collector failed.' % item('error')
+    scraperwiki.status('error', 'Collection failed.')
+
+
